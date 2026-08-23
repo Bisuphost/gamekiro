a6144dfec37ac77bb5604e26120b0dbf825559fc
Sambhav Acharya <work.sambhavacharya@gmail.com>
2026-08-23 14:11:26 +0545

    completed phase 1A iii

--- forum/urls.py
+++ forum/urls.py
@@ -7,9 +7,15 @@ app_name = "forum"
 urlpatterns = [
     path("forum/", views.category_list, name="category_list"),
     path("forum/<slug:category_slug>/", views.category_detail, name="category_detail"),
+    path("forum/<slug:category_slug>/new/", views.thread_create, name="thread_create"),
     path(
         "forum/<slug:category_slug>/<slug:thread_slug>/",
         views.thread_detail,
         name="thread_detail",
+    ),
+    path(
+        "forum/<slug:category_slug>/<slug:thread_slug>/reply/",
+        views.post_create,
+        name="post_create",
     ),
 ]
