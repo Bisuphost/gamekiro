83097dea758169d1da01a6d9f70eb0c10232800e
prabeshbuilds <prabeshuser407@gmail.com>
2026-08-24 22:36:28 +0545

    Feet:Added the messaging path

--- gamekiro/urls.py
+++ gamekiro/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("messaging.urls")),
     path("", include("accounts.urls")),
     path("", include("core.urls")),
     path('u/', include('accounts.urls')),
