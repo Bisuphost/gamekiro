569e5949d027d3eeda5a92ad32d9c2292a7d789b
prabeshbuilds <prabeshuser407@gmail.com>
2026-08-25 21:24:19 +0545

    feet: Update the Messaging

---
+++ messaging/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = "messaging"
+
+urlpatterns = [
+    path("messages/", views.inbox, name="inbox"),
+    path("messages/users/", views.user_list, name="user-list"),
+    path("messages/unread-count/", views.unread_count, name="unread-count"),
+    path("messages/<str:username>/send/", views.send_message, name="send"),
+    path("messages/<str:username>/", views.conversation, name="conversation"),
+]
